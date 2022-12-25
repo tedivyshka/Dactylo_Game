74e7dea086d12b1eae1a8aff1c7b1410e683536f
Tedi Vyshka <tedi.vyshka@gmail.com>
2022-12-25 09:56:28 +0100

    Added GameNormalSolo starting methods

---
+++ src/main/java/PlayerInput.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import model.Game;
+
+
+public class PlayerInput extends KeyAdapter{
+	private Game game;
+	
+	public PlayerInput(Game game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e){
+		int k = e.getKeyCode();
+		game.keyInput(k);
+	}
+}
