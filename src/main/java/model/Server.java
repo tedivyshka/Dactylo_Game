ccfc3a4e97860345674f094c3e6da6c459501156
Tedi Vyshka <tedi.vyshka@gmail.com>
2022-12-31 16:50:58 +0100

    Cleaned up code in Server.java

--- src/main/java/model/Server.java
+++ src/main/java/model/Server.java
@@ -112,15 +112,14 @@ public class Server {
      */
     public void runServer(int desiredNumClients) throws IOException {
         // Create a server socket and start listening for incoming connections
-        ServerSocket serverSocket = new ServerSocket(serverPort);
-        server = serverSocket;
+        server = new ServerSocket(serverPort);
         clients = new ArrayList<>();
 
         // Accept all clients
         int idCounter = 0;
         while (clients.size() < desiredNumClients) {
             // Accept an incoming connection from a client
-            Socket socket = serverSocket.accept();
+            Socket socket = server.accept();
 
             // Create a new thread for the client
             ClientThread clientThread = new ClientThread(socket, idCounter, this);
